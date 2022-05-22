ae9a54d2f8717aa8c1a8de0c8d8a9f4710231202
lakyulakyu <martieszhidan@gmail.com>
2022-05-22 19:34:10 +0700

    first commit

---
+++ src/modul/pkg3/child.java
@@ -0,0 +1,42 @@
+
+package modul.pkg3;
+
+
+public class child {
+
+    
+    public static void ayah(String[] args) {
+ System.out.println("Hello");
+        System.out.println("Selamat belajar di program java");
+    }
+
+    void myname() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void gender() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void nomor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void bagian() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void gaji() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void gaji(String baik) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
+        
+  
+
+        
+    
+    
---
+++ src/modul/pkg3/main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul.pkg3;
+public class main extends mainApp{
+    public static void main(String[] args) {
+
+        child test = new child();
+        test.myname();
+        test.gender();
+        test.nomor();
+        test.bagian();
+        test.gaji();
+        test.gaji("baik");
+}}
